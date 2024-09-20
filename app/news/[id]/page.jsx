64b3c070af7af781a09c6449a42ce1203c8c55a4
rimaafrikyan <afrikyanrima@gmail.com>
2024-09-20 10:30:30 +0400

    error is fixed

--- app/news/[id]/page.jsx
+++ app/news/[id]/page.jsx
@@ -1,27 +1,37 @@
 import SinglePage from "@/components/blogsinglepage/SinglePage";
 import axios from "axios";
-import {address} from "@/repetitiveVariables/variables";
+import { address } from "@/repetitiveVariables/variables";
 
 export async function generateMetadata({ params }) {
     const id = params.id;
-    const {data} = await axios.get(`${address}/news/getOne/${id}`);
-    return {
-        title: data.title,
-        description: 'Հետախույզ լրատվական',
-        openGraph: {
+
+    try {
+        const { data } = await axios.get(`${address}/news/getOne/${id}`);
+        return {
             title: data.title,
             description: 'Հետախույզ լրատվական',
-            url: `https://hetakhuyz.am/news/${id}`,
-            images: `${address}/${data.img}`,
-        },
-        twitter: {
-            card: 'summary_large_image',
-            title: data.title,
+            openGraph: {
+                title: data.title,
+                description: 'Հետախույզ լրատվական',
+                url: `https://hetakhuyz.am/news/${id}`,
+                images: `${address}/${data.img}`,
+            },
+            twitter: {
+                card: 'summary_large_image',
+                title: data.title,
+                description: 'Հետախույզ լրատվական',
+                images: [`${address}/${data.img}`],
+            },
+        };
+    } catch (error) {
+        console.error('Error fetching metadata:', error);
+        return {
+            title: 'Հետախույզ լրատվական',
             description: 'Հետախույզ լրատվական',
-            images: [`${address}/${data.img}`],
-        },
+        };
     }
 }
+
 function Page(props) {
     return (
         <SinglePage/>
